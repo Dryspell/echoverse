3528460347f529fc3317fe32ee592ef660e5c0ea
Dryspell <Nhwinslow@gmail.com>
2024-03-09 19:24:21 -0600

    Add presence

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,6 +14,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as presence from "../presence.js";
 import type * as stripe from "../stripe.js";
 import type * as users from "../users.js";
 import type * as utils from "../utils.js";
@@ -27,6 +28,7 @@ import type * as utils from "../utils.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  presence: typeof presence;
   stripe: typeof stripe;
   users: typeof users;
   utils: typeof utils;
--- convex/schema.ts
+++ convex/schema.ts
@@ -15,4 +15,21 @@ export default defineSchema({
     .index("by_token", ["tokenIdentifier"])
     .index("by_userid", ["id"])
     .index("by_subscriptionId", ["subscriptionId"]),
+    
+  presence: defineTable({
+    user: v.string(),
+    room: v.string(),
+    updated: v.number(),
+  })
+    // Index for fetching presence data
+    .index("room_updated", ["room", "updated"])
+    // Index for updating presence data
+    .index("user_room", ["user", "room"]),
+
+  messages: defineTable({
+    user: v.string(),
+    room: v.string(),
+    message: v.string(),
+    timestamp: v.number(),
+  }),
 });
